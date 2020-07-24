0381e3ccd8fe88679712e8bd4e228bb214399c32
Deleranax <reazer18@gmail.com>
2020-07-24 22:23:10 +0200

    Initial commit

---
+++ installer-template.lua
@@ -0,0 +1,6 @@
+local repositoryName = "OC-Invoice"
+local ownerName = "Deleranax"
+local branch = "master"
+local baseUrl = "https://raw.githubusercontent.com/"
+
+local manifestUrl = baseUrl.."/"..ownerName.."/"..repositoryName.."/"..branch.."/manifest.txt"
