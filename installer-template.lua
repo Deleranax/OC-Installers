89469ed88de05e10ed320aaaf4299ac51fa9d0ca
Deleranax <reazer18@gmail.com>
2020-07-24 22:42:07 +0200

    Update installer-template.lua

--- installer-template.lua
+++ installer-template.lua
@@ -1,6 +1,14 @@
+local term = require("component")
+local component = require("component")
+local net = component.internet
+local gpu = component.gpu
+
 local repositoryName = "OC-Invoice"
 local ownerName = "Deleranax"
 local branch = "master"
 local baseUrl = "https://raw.githubusercontent.com/"
 
 local manifestUrl = baseUrl.."/"..ownerName.."/"..repositoryName.."/"..branch.."/manifest.txt"
+
+function initScreen()
+    
